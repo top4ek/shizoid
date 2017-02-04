f2c78e3ebd9020001570c9bfb03aa61b6f631b2c
Alexander Aleschenko <alexander@aleschenko.ru>
2017-02-04 10:01:51 +0300

    Version bump

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 set :rails_env, 'production'
-server "#{fetch :server_address}", user: "#{fetch :deploy_user}", roles: %w{app db}
+server "#{fetch :server_address}", user: "#{fetch :deploy_user}", roles: %w{app db}, port: 8322
---
+++ config/deploy/production.rb.example
@@ -0,0 +1,2 @@
+set :rails_env, 'production'
+server "#{fetch :server_address}", user: "#{fetch :deploy_user}", roles: %w{app db}
