46a1999aeed65f9b7d7d866581008b61b87add36
Alexander Aleschenko <alexander@aleschenko.ru>
2018-01-29 18:56:21 +0300

    Eightball [C] Fix locale

--- app/controllers/concerns/eightball.rb
+++ app/controllers/concerns/eightball.rb
@@ -1,7 +1,7 @@
 module Eightball
   def eightball(*text)
     return unless can_reply?
-    reply_text = t('.empty').sample
+    reply_text = t('shizoid.eightball.empty').sample
     return reply_with :message, text: reply_text if text.empty?
     case text.first
     when 'enable', 'on'
@@ -11,7 +11,7 @@ module Eightball
       @chat.update(eightball: false)
       reply_text = ok
     else
-      answers = t('.answers')
+      answers = t('shizoid.eightball.answers')
       message = payload.text.downcase
       digest = Digest::SHA1.hexdigest(message).to_i(16) - Date.today.to_time.to_i.div(100) - from.id
       answer_id = digest.divmod(answers.count)[1]
