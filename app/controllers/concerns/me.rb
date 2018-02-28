1eb06020a5a5718cd6a4a5e2987e5ccb2c150d18
Alexander Aleschenko <alexander@aleschenko.ru>
2018-02-28 15:06:33 +0300

    Me [C] Use HTML instead Markdown

--- app/controllers/concerns/me.rb
+++ app/controllers/concerns/me.rb
@@ -1,9 +1,9 @@
 module Me
   def me(*text)
     return unless can_reply?
-    reply = "*#{from.username || from.first_name || from.last_name}* "
+    reply = "<b>#{from.username || from.first_name || from.last_name}<b> "
     reply += text.any? ? text.join(' ') : t('.').sample
-    respond_with :message, text: reply, parse_mode: :markdown
+    respond_with :message, text: reply, parse_mode: :html
     bot.delete_message(chat_id: @chat.telegram_id, message_id: payload.message_id) if can_delete?
   end
 end
